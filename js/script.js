f83898a89243ffb7004fd6c7314d362c5b155694
Jose Gonzalez <jose.gonzalezz0808@gmail.com>
2019-04-26 01:18:01 +0000

    sound nation

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@
 
 console.log('hi');
 
-$('h1').click(function(){
+$('h1').click(function() {
     $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+    $('h1').text('Sound Nation');
+});
+
+$("#search-button").click(function() {
+    var input = $("#search-term").val();
+    var SoundCloud = "https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+    var random = Math.floor(Math.random() * 25);
+
+
+    $.ajax({
+        url: SoundCloud,
+        method: "GET",
+        success: function(response) {
+        response.forEach(function(track){
+                console.log(track.title);
+            $("ul").append("<li>" + track.title + "<li>");
+
+            });
+        }
+    });
 });
