c4f1bf6046bbac7114e70804049151ceb41e38fa
Jose Gonzalez <jose.gonzalezz0808@gmail.com>
2019-05-03 01:35:59 +0000

    Air Sound css

--- js/script.js
+++ js/script.js
@@ -9,19 +9,19 @@ $('h1').click(function() {
 
 $("#search-button").click(function() {
     var input = $("#search-term").val();
-    var SoundCloud = "https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344";
-    var random = Math.floor(Math.random() * 25);
-
-
+    var SoundCloud = "https://api.soundcloud.com/tracks?q=" + input + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+   
+   
     $.ajax({
         url: SoundCloud,
         method: "GET",
         success: function(response) {
-        response.forEach(function(track){
-                console.log(track.title);
-            $("ul").append("<li>" + track.title + "<li>");
+            var random = Math.floor(Math.random() * response.length);
+            console.log(random)
+            var track= response[random]
+            console.log(track)
+            $("ul").append("<li><a>" + track.title + "<a><li>");
 
-            });
         }
     });
 });
